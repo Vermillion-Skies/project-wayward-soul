c8c7c5ce12a482d6850dd4aae749e012407ddaa8
Vermillion-Skies <brtodd6825@eagle.fgcu.edu>
2026-03-13 10:53:20 -0400

    Updated dialog to be more properly punctuated throughout

--- GameFiles/Prologue.py
+++ GameFiles/Prologue.py
@@ -3,8 +3,8 @@ import os #Imports the OS library to check and load files
 import Scripts #Imports custom script library
 #For all intents and purposes, prologue is considered feature complete
 Scripts.screenclear() #the initial game logic that runs at the start
-Scripts.sprint("Welcome, adventurer, to the game")
-Scripts.sprint("This game is a work of fiction. Any resemblences to any person, living or dead, is completely coincidental")
+Scripts.sprint("Welcome, adventurer, to the game.")
+Scripts.sprint("This game is a work of fiction. Any resemblences to any person, living or dead, is completely coincidental.")
 Scripts.sprint("Only by agreeing to these rules will you be allowed to continue into the game.")
 activeinput = 1
 while activeinput == 1:
@@ -24,8 +24,8 @@ while activeinput == 1:
         pass
     pass
 print("")
-Scripts.sprint("The contract has been signed")
+Scripts.sprint("The contract has been signed.")
 Scripts.sprint("As such, the journey may begin.")
-Scripts.sprint("Best of luck, oh lost one")
+Scripts.sprint("Best of luck, oh lost one.")
 answer = str(input("(press enter to continue)"))
 subprocess.run(["python", "GameFiles/ChapterOne.py"]) #Loads the chapter 1 python file. This will not work in VSC for some reason, but it does when you just run the file normally. Idk why
