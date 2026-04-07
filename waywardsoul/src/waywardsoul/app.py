fcc4d60db61c296d87cf09f36e38c8482e342c76
Vermillion-Skies <mario4672yt@gmail.com>
2026-04-07 12:23:53 -0400

    RW-upd: Created initial window design

--- waywardsoul/src/waywardsoul/app.py
+++ waywardsoul/src/waywardsoul/app.py
@@ -1,11 +1,75 @@
 import toga
 from toga.style.pack import COLUMN, ROW
-
+from toga.style import Pack
+from toga.constants import Direction
 
 class WaywardSoul(toga.App):
     def startup(self):
         main_box = toga.Box()
-
+        logobox = toga.Box(
+            style=Pack(
+                flex=1
+            )
+        )
+        titlebox = toga.Box(
+            style=Pack(
+                flex=1
+            )
+        )
+        titlebox.add(
+            toga.Label(
+                "Wayward Soul",
+                margin=(0,5)
+            )
+        )
+        uppersplit = toga.SplitContainer(
+            content=[
+                (logobox, 9),
+                (titlebox, 1)
+            ],
+            direction=Direction.HORIZONTAL
+        )
+        mainbuttons = toga.Box(
+            style=Pack(
+                flex=1
+            )
+        )
+        ngb = toga.Button(
+            "New Game",
+            #on_press=self.newgame,
+            margin=5
+        )
+        lgb = toga.Button(
+            "Load Game",
+            #on_press=self.loadgame,
+            margin=5
+        )
+        cfb = toga.Button(
+            "Config",
+            #on_press=self.configmenu,
+            margin=5
+        )
+        qtb = toga.Button(
+            "Quit",
+            #on_press=self.quit,
+            margin=5
+        )
+        mainbuttons.add(
+            ngb,
+            lgb,
+            cfb,
+            qtb
+        )
+        wholesplit = toga.SplitContainer(
+            content=[
+                (uppersplit, 9),
+                (mainbuttons, 1)
+            ],
+            direction=Direction.HORIZONTAL
+        )
+        main_box.add(
+            wholesplit
+        )
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
